6dc5d9d73131e25fb9b09643b6d7d47b28c5bcd9
Badulescu Andrei <d3iu25@gmail.com>
2023-11-22 21:20:36 +0200

    backend url

--- src/api-helpers/api.js
+++ src/api-helpers/api.js
@@ -3,7 +3,6 @@ import axios from 'axios';
 const api = axios.create({
   baseURL: 'https://travel-backend-shv1.onrender.com',
   withCredentials: true,
-  //baseURL: 'https://travel-backend-shv1.onrender.com'
 });
 
 export default api;
