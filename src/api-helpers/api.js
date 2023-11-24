8235236c58eec36880dc29b6894774cbb8e987ff
Deiu <d3iu25@gmail.com>
2023-11-24 10:34:13 +0200

    server link 2

--- src/api-helpers/api.js
+++ src/api-helpers/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://travel-backend-shv1.onrender.com',
+  baseURL: 'http://backend.dinhub.ro',
   withCredentials: true,
 });
 
