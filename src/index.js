24406b95fd3c0094857e12206b71278f78d1ae74
Badulescu Andrei <d3iu25@gmail.com>
2023-12-01 13:02:56 +0200

    get user status error and create user steps

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
+import { ToastContainer } from "react-toastify";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import axios from "axios";
@@ -15,6 +16,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
+    <ToastContainer />
       <Provider store={store}>
         <App />
       </Provider>
